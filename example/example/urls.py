9b069776142b0d92be6a23bcef725be6b67b0ec0
Sergio <sergiorodriguezcalvo@gmail.com>
2015-02-23 21:39:38 +0100

    Importada aplicacion para llamada a las vistas

--- example/example/urls.py
+++ example/example/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from ws import views
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'example.views.home', name='home'),
