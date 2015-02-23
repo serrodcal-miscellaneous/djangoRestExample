78c45a7d0571955710d325f621249a4a4bd50eff
Sergio <sergiorodriguezcalvo@gmail.com>
2015-02-23 21:34:38 +0100

    Eliminado campo id del modelo proque ya lo hereda de Model

--- example/ws/models.py
+++ example/ws/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
 class Departamento(models.Model):
-    id = models.IntegerField(null=False)
     nombre = models.CharField(max_length=100)
 
 class Empleado(models.Model):
-    id = models.IntegerField(null=False)
     nombre = models.CharField(max_length=100)
     departamento = models.ForeignKey(Departamento)
